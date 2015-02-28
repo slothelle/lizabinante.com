9451cbc1bab4c2206a9bfc05609e6f0a5c36e1b3
Liz Abinante <eabinante@gmail.com>
2015-02-27 19:36:29 -0800

    blog

--- config.rb
+++ config.rb
@@ -1,4 +1,38 @@
 ###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+  blog.name = "liz abinante"
+
+  blog.permalink = "{category}/{year}/{title}/"
+  # Matcher for blog source files
+  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "post"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+###
 # Compass
 ###
 
@@ -36,9 +70,9 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -52,6 +86,14 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
+
+set :haml, { :ugly => true, :format => :html5 }
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
+activate :blog do |blog|
+  # set options on blog
+end
 
 # Build-specific configuration
 configure :build do
---
+++ source/posts/2014-08-22-unicorns-are-people-too.html.markdown
@@ -0,0 +1,12 @@
+---
+title:  "Unicorns Are People, Too: Re-Thinking Soft and Hard Skills"
+date:   2014-08-22 07:22:43
+tags: talk
+category: talk
+conf: "Madison Ruby"
+linky: "http://madisonpl.us/ruby/"
+location: "Madison, WI"
+speakerdeck: "https://speakerdeck.com/feministy/unicorns-are-people-too-re-thinking-soft-and-hard-skills-1"
+---
+
+As developers, we tend to value hard skills that can be quantified or measured objectively. Job postings search for unicorns, but we are people first and foremost and being human isn’t as easy as programming. While the code comes easily, the soft skills that make us human are complicated and difficult to get right. This talk will explore the danger of neglecting so-called “soft” skills, what we stand to lose by overvaluing technical skills, and alternatives to the hard and soft dichotomy.
